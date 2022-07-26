4961f1e4e217ef323beebd11f0af19be3759d8bc
Kanamori Rui <oscarpeterson0509@gmail.com>
2022-07-27 08:25:23 +0900

    cleanup

--- src/main/java/io/github/daytimepapaya/tools/RssAdder.java
+++ src/main/java/io/github/daytimepapaya/tools/RssAdder.java
@@ -1,13 +1,18 @@
 package io.github.daytimepapaya.tools;
 
 import io.github.daytimepapaya.util.DatabaseUtils;
+import org.jdbi.v3.core.statement.PreparedBatch;
+
+import java.util.Arrays;
 
 public class RssAdder {
     public static void main(String[] args) {
         var jdbi = DatabaseUtils.getJdbi();
         jdbi.useHandle(handle -> {
             handle.execute("create table if not exists rss (id integer primary key autoincrement, url text unique)");
-            handle.execute("insert or ignore into rss(url) values(?)", args[0]);
+            PreparedBatch batch = handle.prepareBatch("insert or ignore into rss(url) values(:url)");
+            Arrays.stream(args).forEach(e -> batch.bind("url", e).add());
+            batch.execute();
         });
     }
 }
